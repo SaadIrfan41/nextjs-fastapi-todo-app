13f135817eba3dcefdaa7a9233859ee0d1ff7239
saix511 <saadirfan41@gmail.com>
2024-01-19 04:10:10 +0500

    .

--- api/index.py
+++ api/index.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from api.db import modals
 from api.db.db_connection import engine
 from api.router import auth, todo
 
+# from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
 modals.Base.metadata.create_all(bind=engine)
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
